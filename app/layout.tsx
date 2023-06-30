2adf15f883d52a62343be7f1d24a82d9793b8839
adenugbamichael <adenugbamicke@gmail.com>
2023-06-30 11:16:52 +0100

    Connect prisma to mongodb, connect to db and create register and login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import getCurrentUser from "./actions/getCurrentUser"
+import LoginModal from "./components/modals/LoginModal"
 import RegisterModal from "./components/modals/RegisterModal"
 import Navbar from "./components/navbar/Navbar"
 import "./globals.css"
@@ -8,17 +10,19 @@ export const metadata = {
   description: "Airbnb application",
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser()
   return (
     <html lang='en' suppressHydrationWarning={true}>
       <body suppressHydrationWarning={true}>
         <ToasterProvider />
+        <LoginModal />
         <RegisterModal />
-        <Navbar />
+        <Navbar currentUser={currentUser} />
         {children}
       </body>
     </html>
