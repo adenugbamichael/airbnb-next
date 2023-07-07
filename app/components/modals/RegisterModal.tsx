e645f57e974d7125b131e79d02cd76027a7c9421
adenugbamichael <adenugbamicke@gmail.com>
2023-07-07 17:00:06 +0100

    fix input

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -12,7 +12,7 @@ import useLoginModal from "@/app/hooks/useLoginModal"
 import useRegisterModal from "@/app/hooks/useRegisterModal"
 
 import Modal from "./Modal"
-import Input from "../inputs/Input"
+import Input from "../inputs/input"
 import Heading from "../Heading"
 import Button from "../Button"
 
